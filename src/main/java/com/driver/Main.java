59aac20db384b498aeecf6396339d5310bfe3344
sayed-mudassir <sayedmuddassir68@gmail.com>
2023-08-13 23:58:06 +0530

    Attempt1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x ,int y){
+            return x*y;
+        }
+        public int product(int x ,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x ,double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(2,5);
+        p.product(4,5,7);
+        p.product(8.5,9.7);
+    }
+
 
 }
